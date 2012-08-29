f8ee886926f6690d9f63673c5eeadff552a65941
robertotomasi <rob.tomasi@gmail.com>
2012-08-29 14:30:32 +0200

    Commit iniziale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Corehab::Application.config.secret_token = '614233e1eeacb741272d0c78271616cd8740ef2739ad3823e6c24eedd67fee67f6207fa94dd8e030a99f3c892f3f42d7748e34a00c30e4144fb0def304582af4'
